76a29516d14c58418a3a137a1ac7eadc562ba380
Md. Mahmudur Rahman <mamun4122@gmail.com>
2016-07-22 19:13:05 +0600

    algorithm

--- lightoj/1424.cpp
+++ lightoj/1424.cpp
@@ -1,3 +1,7 @@
+#pragma comment(linker, "/stack:640000000")
+
+#include <algorithm>
+#include <bitset>
 #include <cassert>
 #include <cctype>
 #include <climits>
@@ -5,72 +9,72 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <fstream>
 #include <iostream>
-#include <sstream>
 #include <iomanip>
-#include <string>
-#include <vector>
-#include <list>
-#include <set>
-#include <map>
-#include <stack>
-#include <queue>
-#include <algorithm>
 #include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
 #include <utility>
-#include <ctime>
+#include <vector>
 using namespace std;
-
-
-#define mem(p,v) memset(p, v, sizeof(p))
-#define SET(p) 	  memset(p, -1, sizeof(p))
-#define CLR(p)    memset(p, 0, sizeof(p))
-
-
-#define    getI(a) 	         scanf("%d", &a)
-#define    getII(a,b) 	     scanf("%d%d", &a, &b)
-#define    getIII(a,b,c)     scanf("%d%d%d", &a, &b, &c)
-#define    getL(n)           scanf("%lld",&n)
-#define    getF(n)           scanf("%lf",&n)
-
-#define rep(i,n) for( i = 1 ; i<=(n) ; i++)
-
-#define ALL(p)  p.begin(),p.end()
-#define ALLR(p) p.rbegin(),p.rend()
-#define READ(f) freopen(f, "r", stdin)
-#define WRITE(f) freopen(f, "w", stdout)
-#define pb(x) push_back(x)
-
-#define    vi 	 vector < int >
-#define    vii 	 vector < vector < int > >
-#define    pii 	 pair< int, int >
-#define    psi 	 pair< string, int >
-#define    ff 	 first
-#define    ss 	 second
-#define    ll	 long long
-#define    ull 	 unsigned long long
-#define    ui    unsigned int
-#define    ld 	 long double
-
 
 const double EPS = 1e-9;
 const int INF = 0x7f7f7f7f;
 const double PI=acos(-1.0);
 
-#define iseq(a,b) (fabs(a-b)<EPS)
+#define    READ(f) 	         freopen(f, "r", stdin)
+#define    WRITE(f)   	     freopen(f, "w", stdout)
+#define    MP(x, y) 	     make_pair(x, y)
+#define    PB(x)             push_back(x)
+#define    rep(i,n)          for(int i = 1 ; i<=(n) ; i++)
+#define    repI(i,n)         for(int i = 0 ; i<(n) ; i++)
+#define    FOR(i,L,R) 	     for (int i = L; i <= R; i++)
+#define    ROF(i,L,R) 	     for (int i = L; i >= R; i--)
+#define    FOREACH(i,t)      for (typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
+#define    ALL(p) 	         p.begin(),p.end()
+#define    ALLR(p) 	         p.rbegin(),p.rend()
+#define    SET(p) 	         memset(p, -1, sizeof(p))
+#define    CLR(p)            memset(p, 0, sizeof(p))
+#define    MEM(p, v)         memset(p, v, sizeof(p))
+#define    getI(a) 	         scanf("%d", &a)
+#define    getII(a,b) 	     scanf("%d%d", &a, &b)
+#define    getIII(a,b,c)     scanf("%d%d%d", &a, &b, &c)
+#define    getL(a)           scanf("%lld",&a)
+#define    getLL(a,b)        scanf("%lld%lld",&a,&b)
+#define    getLLL(a,b,c)     scanf("%lld%lld%lld",&a,&b,&c)
+#define    getC(n)           scanf("%c",&n)
+#define    getF(n)           scanf("%lf",&n)
+#define    getS(n)           scanf("%s",n)
+#define    bitCheck(a,k)     ((bool)(a&(1<<(k))))
+#define    bitOff(a,k)       (a&(~(1<<(k))))
+#define    bitOn(a,k)        (a|(1<<(k)))
+#define    bitFlip(a,k)      (a^(1<<(k)))
+#define    iseq(a,b)         (fabs(a-b)<EPS)
+#define    vi 	 vector < int >
+#define    vii 	 vector < vector < int > >
+#define    pii 	 pair< int, int >
+#define    ff 	 first
+#define    ss 	 second
+#define    ll	 long long
+#define    ull 	 unsigned long long
 
 template< class T > inline T _abs(T n) { return ((n) < 0 ? -(n) : (n)); }
 template< class T > inline T _max(T a, T b) { return (!((a)<(b))?(a):(b)); }
 template< class T > inline T _min(T a, T b) { return (((a)<(b))?(a):(b)); }
+template< class T > inline T _swap(T &a, T &b) { a=a^b;b=a^b;a=a^b;}
 template< class T > inline T gcd(T a, T b) { return (b) == 0 ? (a) : gcd((b), ((a) % (b))); }
 template< class T > inline T lcm(T a, T b) { return ((a) / gcd((a), (b)) * (b)); }
+template <typename T> string NumberToString ( T Number ) { ostringstream ss; ss << Number; return ss.str(); }
 
-
-
-//******************DELETE****************
-#define mam
 #ifdef mamun
-     #define debug(args...) {dbg,args; cerr<<endl;}
+     #define debug(args...) {cerr<<"*: "; dbg,args; cerr<<endl;}
 #else
     #define debug(args...)  // Just strip off all debug tokens
 #endif
@@ -81,10 +85,10 @@ struct debugger{
         return *this;
     }
 }dbg;
-//******************DELETE****************
-#define mx 30005
-
+///****************** template ends here ****************
+#define mx 2005
 int arr[mx];
+
 struct info
 {
     int data,ind;
@@ -125,42 +129,49 @@ info query(int node,int b,int e,int i,int j)
     }
     return p1;
 }
-int ans,n;
+int ans,n,m;
 void recur(int i,int j)
 {
     if(i>j)return;
-    //debug(i,j,query(1,1,n,i,j));
-    info val=query(1,1,n,i,j);
+//    debug(i,j,query(1,1,n,i,j));
+    info val=query(1,1,m,i,j);
     ans=max(ans,val.data*(j-i+1));
     recur(i,val.ind-1);
     recur(val.ind+1,j);
 
 }
 
-
+char st[2005];
 int main() {
     //READ("in.txt");
     //WRITE("out.txt");
-    int t,cs,i;
+    int t;//cs;//,i,j;
     getI(t);
     rep(cs,t)
     {
         printf("Case %d:",cs);
-        getI(n);
-        CLR(arr);CLR(tree);
-        rep(i,n)getI(arr[i]);
-        init(1,1,n);
+        getII(n,m);
+        CLR(arr);
+        getchar();
         ans=0;
-        recur(1,n);
+        rep(i,n)
+        {
+            gets(st);
+//            debug(st);
+            repI(j,m)
+            {
+                if(st[j]=='0')arr[j+1]++;
+                else arr[j+1]=0;
+            }
+            init(1,1,m);
+//            debug("")
+            recur(1,m);
+        }
         printf(" %d\n",ans);
 
     }
-
-
-
-
-
     return 0;
 }
+
 
 
