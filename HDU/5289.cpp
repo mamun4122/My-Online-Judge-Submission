b8c69be721ea21a1a6466570ca9a0c365a5e9264
Md. Mahmudur Rahman <mamun4122@gmail.com>
2016-08-15 06:03:15 +0600

    segment tree

---
+++ HDU/5289.cpp
@@ -0,0 +1,152 @@
+#pragma comment(linker, "/stack:640000000")
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+
+const double EPS = 1e-9;
+const int INF = 0x7f7f7f7f;
+const double PI=acos(-1.0);
+
+#define    READ(f) 	         freopen(f, "r", stdin)
+#define    WRITE(f)   	     freopen(f, "w", stdout)
+#define    MP(x, y) 	     make_pair(x, y)
+#define    PB(x)             push_back(x)
+#define    rep(i,n)          for(int i = 1 ; i<=(n) ; i++)
+#define    repI(i,n)         for(int i = 0 ; i<(n) ; i++)
+#define    FOR(i,L,R) 	     for (int i = L; i <= R; i++)
+#define    ROF(i,L,R) 	     for (int i = L; i >= R; i--)
+#define    FOREACH(i,t)      for (typeof(t.begin()) i=t.begin(); i!=t.end(); i++)
+#define    ALL(p) 	         p.begin(),p.end()
+#define    ALLR(p) 	         p.rbegin(),p.rend()
+#define    SET(p) 	         memset(p, -1, sizeof(p))
+#define    CLR(p)            memset(p, 0, sizeof(p))
+#define    MEM(p, v)         memset(p, v, sizeof(p))
+#define    getI(a) 	         scanf("%d", &a)
+#define    getII(a,b) 	     scanf("%d%d", &a, &b)
+#define    getIII(a,b,c)     scanf("%d%d%d", &a, &b, &c)
+#define    getL(a)           scanf("%lld",&a)
+#define    getLL(a,b)        scanf("%lld%lld",&a,&b)
+#define    getLLL(a,b,c)     scanf("%lld%lld%lld",&a,&b,&c)
+#define    getC(n)           scanf("%c",&n)
+#define    getF(n)           scanf("%lf",&n)
+#define    getS(n)           scanf("%s",n)
+#define    bitCheck(a,k)     ((bool)(a&(1<<(k))))
+#define    bitOff(a,k)       (a&(~(1<<(k))))
+#define    bitOn(a,k)        (a|(1<<(k)))
+#define    bitFlip(a,k)      (a^(1<<(k)))
+#define    iseq(a,b)         (fabs(a-b)<EPS)
+#define    vi 	 vector < int >
+#define    vii 	 vector < vector < int > >
+#define    pii 	 pair< int, int >
+#define    ff 	 first
+#define    ss 	 second
+#define    ll	 long long
+#define    ull 	 unsigned long long
+
+template< class T > inline T _abs(T n) { return ((n) < 0 ? -(n) : (n)); }
+template< class T > inline T _max(T a, T b) { return (!((a)<(b))?(a):(b)); }
+template< class T > inline T _min(T a, T b) { return (((a)<(b))?(a):(b)); }
+template< class T > inline T _swap(T &a, T &b) { a=a^b;b=a^b;a=a^b;}
+template< class T > inline T gcd(T a, T b) { return (b) == 0 ? (a) : gcd((b), ((a) % (b))); }
+template< class T > inline T lcm(T a, T b) { return ((a) / gcd((a), (b)) * (b)); }
+template <typename T> string NumberToString ( T Number ) { ostringstream ss; ss << Number; return ss.str(); }
+
+
+///****************** template ends here ****************
+int t,n,m;
+#define MAX 100005
+int arr[MAX];
+
+
+pii tree[MAX*3];
+pii call(pii a,pii b)
+{
+    pii tmp;
+    tmp.ff=max(a.ff,b.ff);
+    tmp.ss=min(a.ss,b.ss);
+    return tmp;
+}
+
+void init(int node,int b,int e)
+{
+	if(b==e)
+	{
+		tree[node].ff=arr[b];
+		tree[node].ss=arr[b];
+		return;
+	}
+	int Left=node*2;
+	int Right=node*2+1;
+	int mid=(b+e)/2;
+	init(Left,b,mid);
+	init(Right,mid+1,e);
+	tree[node]=call(tree[Left],tree[Right]);
+}
+pii query(int node,int b,int e,int i,int j)
+{
+	if (i > e || j < b)	return pii(-INF,INF);
+	if (b >= i && e <= j) return tree[node];
+	int Left=node*2;
+	int Right=node*2+1;
+	int mid=(b+e)/2;
+	pii p1=query(Left,b,mid,i,j);
+	pii p2=query(Right,mid+1,e,i,j);
+	return call(p1,p2);
+}
+
+
+int main() {
+    ///check for 0 or -1 if input not specified
+    #ifdef mamun
+//        READ("in.txt");
+//        WRITE("out.txt");
+    #endif // mamun
+//    ios_base::sync_with_stdio(0);cin.tie(0);
+    getI(t);
+    rep(cs,t)
+    {
+        getII(n,m);
+        rep(i,n)getI(arr[i]);
+        init(1,1,n);
+        ll ans=0;
+        int lft=1,rght=1;
+        while(rght<=n)
+        {
+            pii val=query(1,1,n,lft,rght);
+            while(lft<=rght&&abs(val.ff-val.ss)>=m)
+            {
+                lft++;
+                val=query(1,1,n,lft,rght);
+            }
+            ans+=(ll)(rght-lft+1);
+            rght++;
+        }
+        printf("%lld\n",ans);
+
+    }
+
+
+    return 0;
+}
